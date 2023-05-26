fbb948ef4f3eea5ff210d6a43c59ddfabfaeca52
jcmontano-snowflake04 <134660603+jcmontano-snowflake04@users.noreply.github.com>
2023-05-26 10:42:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,10 @@ streamlit.dataframe(fruits_to_show)
 #https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.loc.html 
 
 streamlit.header('Fruityvice Fruit Advice!')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered', fruit_choice) 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/+fruit_choice")
 #streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
