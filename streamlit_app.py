82bf80d1e6611f9dbf678f6fec6aef5dfd75dfd3
jcmontano-snowflake04 <134660603+jcmontano-snowflake04@users.noreply.github.com>
2023-05-26 11:47:23 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ streamlit.dataframe(fruits_to_show)
 #https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.loc.html 
 
 def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
 
